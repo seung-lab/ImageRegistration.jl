d34558250b6cf36a7006483d435ac48595d1c4d5
Thomas Macrina <tmacrina@princeton.edu>
2015-08-08 22:45:55 -0400

    Use this repo in my bucket folder to find the test images needed for the demo/load methods. See load_mesh in piecewiseaffine_render to demonstrate a piecewise affine transform applied to a full size tile. Rewrote the mesh generation method to be much quicker in ImageView. Rewrote some methods from PiecewiseAffine package in piecewiseaffine_warp.jl to run much quicker. Warping takes 45-60s now on that fullsize resolution tile.

--- imwarp.jl
+++ imwarp.jl
@@ -5,67 +5,103 @@ using Grid
 using Base.Test
 
 type Rect
-  x::Float64
-  y::Float64
+  i::Float64
+  j::Float64
   h::Float64
   w::Float64
 end
 
 type SpatialRef
-  x::Float64
-  y::Float64
+  i::Float64
+  j::Float64
 end
 
-function rect2pts(rect::Rect)
-  pts = [rect.x rect.y;
-        rect.x + rect.h rect.y;
-        rect.x + rect.h rect.y + rect.w;
-        rect.x rect.y + rect.w];
-  return pts
-end
-
-function transform_images(imgA, imgB, tformA, tformB)
-
+function rect2pts(r)
+  # Create polygon point list, counter-clockwise
+  return [r.i r.j;
+          r.i+r.h r.j;
+          r.i+r.h r.j+r.w;
+          r.i r.j+r.w;
+          r.i r.j];
 end
 
 # Apply geometric transform to image
 function imwarp(img, tform)
-  img_size = size(img)
-  data = data(img)
+  bb = sz2bb(size(img))
+  tbb = snap_bb(tform_bb(bb, tform))
+  out_img = Array(Int64, Int64(tbb.h), Int64(tbb.w))
 
+  i = 1:size(img,1)
+  j = 1:size(img,2)
+  zi = CoordInterpGrid((i,j), img.data, 0.0, InterpLinear)
 
+  i_off = Int64(tbb.i)
+  j_off = Int64(tbb.j)
+
+  for i = 1:Int64(bb.h)
+    for j = 1:Int64(bb.w)
+      pt = [i+i_off j+j_off 1]
+      ipt = (pt*tform^-1)[:,1:2]
+      out_img[i, j] = zi[ipt...]
+    end
+  end  
+  return out_img
 end
 
-function warp_bb(bb, tform)
-  rect_pts = hcat(rect2pts(bb), ones(Int64, 4,1))
-  tform_pts = rect_pts * tform
-  x = minimum(tform_pts[:, 1])
-  y = minimum(tform_pts[:, 2])
-  h = maximum(tform_pts[:, 1]) - x
-  w = maximum(tform_pts[:, 2]) - y
-  return Rect(x, y, h, w)
+function snap_bb(bb)
+  r = rect2pts(bb)
+  bb.i = floor(bb.i)
+  bb.j = floor(bb.j)
+  h = ceil(r[2,1]) - bb.i
+  w = ceil(r[3,2]) - bb.j
+  return Rect(bb.i, bb.j, h, w)
+end
+
+function tform_bb(bb, tform)
+  bb_pts = hcat(rect2pts(bb), ones(Int64, 5,1))
+  tform_pts = bb_pts * tform
+  i = minimum(tform_pts[:,1])
+  j = minimum(tform_pts[:,2])
+  w = maximum(tform_pts[:,2]) - j
+  h = maximum(tform_pts[:,1]) - i
+  return Rect(i, j, w, h)
 end
 
 function sz2bb(sz)
-  return Rect(0, 0, sz...)
+  return Rect(0, 0, sz...);
 end
 
-function test_warp_bb()
+function test_tform_bb()
   sz = (100, 100)
   tform = [1 0 0;
           0 1 0;
           100 100 1];
   bb = Rect(100, 100, 100, 100)
-  tbb = warp_bb(sz2bb(sz), tform)
+  tbb = tform_bb(sz2bb(sz), tform)
   @test rect2pts(bb) == rect2pts(tbb)
 
   sz = (100, 100)
   tform = [cos(pi/6) -sin(pi/6) 0;
           sin(pi/6) cos(pi/6) 0;
           0 0 1];
-  bb = Rect(0, -50, 136.6, 136.6)
-  tbb = warp_bb(sz2bb(sz), tform)
+  bb = Rect(0, -50, 136.60254037844388, 136.60254037844388)
+  tbb = tform_bb(sz2bb(sz), tform)
+  @test_approx_eq rect2pts(bb) rect2pts(tbb)
+end
+
+function test_snap_bb()
+  bb = snap_bb(sz2bb((100.5, 200.75)))
+  tbb = sz2bb((101, 201))
   @test rect2pts(bb) == rect2pts(tbb)
+
+  bb = snap_bb(sz2bb((100.9, 200.1)))
+  tbb = sz2bb((101, 201))
+  @test rect2pts(bb) == rect2pts(tbb)
+
+  bb = snap_bb(Rect(100.9, 200.1, 100.5, 100.2))
+  tbb = sz2bb((101, 201))
+  tbb = Rect(100, 200, 102, 101)
+  @test rect2pts(bb) == rect2pts(tbb)  
 end
 
 function test_img()
@@ -73,5 +109,6 @@ function test_img()
 end
 
 function test()
-  test_warp_bb()
+  test_tform_bb()
+  test_snap_bb()
 end
--- piecewiseaffine_render.jl
+++ piecewiseaffine_render.jl
@@ -1,42 +1,161 @@
 using HDF5
+using JLD
 using Images
 using ImageView
 using PiecewiseAffineTransforms
 using Color
 using FixedPointNumbers
 
-include("incidence2triangles")
+include("incidence2triangles.jl")
+include("piecewiseaffine_warp.jl")
 
-function xy2ij(shape, height)
-	[height .- shape[:, 2] shape[:, 1]]
-end
+const BUCKET = "/usr/people/tmacrina/seungmount/research/tommy/Julimaps"
+
+xy2ij(shape, height) = [height .- shape[:, 2] shape[:, 1]]
+rawdata(img) = convert(Array{Float64, 2}, data(separate(img)))
 
 function demo_mesh()
-	bucket = "/usr/people/tmacrina/seungmount/research/tommy/Julimaps"
-	file = "11x11.h5"
-	path = joinpath(bucket, file)
-	v = h5read(path, "v") # nodes
-	e = h5read(path, "e") # edges
-	k = h5read(path, "k") # spring constants
-	l = h5read(path, "l") # resting lengths
-	n = h5read(path, "n") # number of edges
-	m = h5read(path, "m") # number of nodes
+    # file = "8000x8000_100px.jld"
+    file = "r4c2-r4c3.jld"
+    path = joinpath(BUCKET, file)
+    d = load(path)
+    v = convert(Array{Float64,2}, d["v"]) # nodes
+    e = convert(Base.SparseMatrix.SparseMatrixCSC{Float64,Int64}, d["e"]) # edges
+    k = d["k"] # spring constants
+    l = d["l"] # resting lengths
+    n = d["n"] # number of edges
+    m = d["m"] # number of nodes
+    incidence = e
+    nodes = v
+    # Display white mesh on black background
+    node_dict = incidence2dict(incidence)
+    triangles = dict2triangles(node_dict)
+    sz = round(Int,maximum(nodes[1,:]))+10, round(Int,maximum(nodes[2,:]))+10
+    img = Image(zeros(Bool, sz...)) # Needs to be Float
+    shape = xy2ij(nodes', size(img, 1))
 
-	display_mesh(e', v')
+    println("Original images and shapes")
+    # triplot(img, shape, triangles)
+    # plot_edges2(img, shape, node_dict)
+    plot_mesh(img, nodes', node_dict)
 end
 
-function display_mesh(incidence, nodes)
-	# Display white mesh on black background
-	triangles = incidence2triangles(incidence)
-	sz = round(Int,maximum(nodes[:,1]))+1, round(Int,maximum(nodes[:,2]))+1
-	img = Image(zeros(sz)) # Needs to be Float
-	shape = xy2ij(nodes, size(img, 1))
+function plot_mesh(img, shape, node_dict)
+    imgc, img2 = view(img)
+    lines = Array(Float64, 4, 0)
+    for k in sort(collect(keys(node_dict)))
+        for v in node_dict[k]
+            a = reverse(vec(shape[k,:]))
+            b = reverse(vec(shape[v,:]))
+            lines = hcat(lines, vcat(a, b))
+        end
+    end
+    annotate!(imgc, img2, AnnotationLines(lines, coord_order="xxyy"))
+end
 
-	println("Original images and shapes")
-	triplot(img, shape, triangles)
+function load_mesh()
+    file = "solvedMesh.jld"
+    path = joinpath(BUCKET, file)
+    d = load(path)
+
+    v = convert(Array{Float64,2}, d["nodes"]) # nodes
+    vt = convert(Array{Float64,2}, d["nodes_t"]) # transformed nodes
+    e = convert(Base.SparseMatrix.SparseMatrixCSC{Float64,Int64}, d["edges"]) # edges
+    # k = d["k"] # spring constants
+    # l = d["l"] # resting lengths
+
+    # XWorldLimits: [2.1907e+04 2.9881e+04]
+    # YWorldLimits: [3.6430e+04 4.4393e+04]
+    initial_nodes = v;
+    final_nodes = vt;
+    initial_nodes[1,:] = v[1,:] - 21906
+    initial_nodes[2,:] = v[2,:] - 36429
+    final_nodes[1,:] = vt[1,:] - 21906
+    final_nodes[2,:] = vt[2,:] - 36429    
+
+    tileA = rawdata(imread(joinpath(BUCKET, "test_images", "Tile_r4-c2_S2-W001_sec20.tif")))
+    tileB = rawdata(imread(joinpath(BUCKET, "test_images", "Tile_r4-c3_S2-W001_sec20.tif")))
+
+    high = Int64(ceil(max(maximum(initial_nodes), maximum(final_nodes))))
+    szA = size(tileA)
+    tileA_padded = vcat(tileA, zeros(high-szA[1], szA[2]))
+    szA = size(tileA_padded)
+    tileA_padded = hcat(tileA_padded, zeros(szA[1], high-szA[2]))
+
+    src_shape = xy2ij(initial_nodes', size(tileA_padded, 1))
+    dst_shape = xy2ij(final_nodes', size(tileA_padded, 1))
+
+    tileA_warped = warp_piecewiseaffine(tileA_padded, e, src_shape, dst_shape)     
+    return tileA, tileA_warped
 end
 
 function warp_piecewiseaffine(img, incidence, initial_nodes, final_nodes)
-	triangles = incidence2triangles(incidence)
-	return pa_warp(img, initial_nodes, final_nodes, triangles)
+    node_dict = incidence2dict(incidence)
+    triangles = dict2triangles(node_dict)
+    warped = pa_warp2(img, initial_nodes, final_nodes, triangles)
+    plot_mesh(img, initial_nodes, node_dict)
+    plot_mesh(warped, final_nodes, node_dict)
+    return warped
+end
+
+function demo_warp2()
+    img = imread(joinpath(BUCKET, "test_images", "turtle.jpg"))
+    img = convert(Array{Float64, 3}, data(separate(img)))
+    initial_nodes = [20.0 20.0;
+                    620.0 20.0;
+                    620.0 560.0;
+                    20.0 560.0;
+                    320.0 290.0]
+    final_nodes = [20.0 20.0;
+                    620.0 20.0;
+                    620.0 560.0;
+                    20.0 560.0;
+                    400.0 460.0]
+    incidence = [1 1 1 0 0 0 0 0;
+                -1 0 0 1 1 0 0 0;
+                0 0 0 -1 0 1 1 0;
+                0 -1 0 0 0 0 -1 1;
+                0 0 -1 0 -1 -1 0 -1]
+    triangles = [1 2 5;
+                1 4 5;
+                2 3 5;
+                3 4 5];
+    src_shape = xy2ij(initial_nodes, size(img, 1))
+    dst_shape = xy2ij(final_nodes, size(img, 1))
+    node_dict = incidence2dict(incidence)
+    plot_mesh(img, src_shape, node_dict)
+
+    warp = pa_warp2(img, src_shape, dst_shape, triangles)
+    plot_mesh(warp, dst_shape, node_dict)
+end
+
+function demo_warp3()
+    img = imread(joinpath(BUCKET, "test_images", "turtle.jpg"))
+    img = convert(Array{Float64, 3}, data(separate(img)))
+    initial_nodes = [20.0 20.0;
+                    620.0 20.0;
+                    620.0 560.0;
+                    20.0 560.0;
+                    320.0 290.0]
+    final_nodes = [20.0 20.0;
+                    620.0 20.0;
+                    620.0 560.0;
+                    20.0 560.0;
+                    400.0 460.0]
+    incidence = [1 1 1 0 0 0 0 0;
+                -1 0 0 1 1 0 0 0;
+                0 0 0 -1 0 1 1 0;
+                0 -1 0 0 0 0 -1 1;
+                0 0 -1 0 -1 -1 0 -1]
+    triangles = [1 2 5;
+                1 4 5;
+                2 3 5;
+                3 4 5];
+    src_shape = xy2ij(initial_nodes, size(img, 1))
+    dst_shape = xy2ij(final_nodes, size(img, 1))
+    node_dict = incidence2dict(incidence)
+    plot_mesh(img, src_shape, node_dict)
+
+    warp = pa_warp3(img, src_shape, dst_shape, triangles)
+    plot_mesh(warp, dst_shape, node_dict)
 end
