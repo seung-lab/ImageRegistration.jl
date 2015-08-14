cf2cf8776f0af437d6b1c0325247112da34e3f44
dodamih <dodam.ih@gmail.com>
2015-08-14 17:34:27 -0400

    added parallelisation

--- MeshSession.jl
+++ MeshSession.jl
@@ -1,51 +1,55 @@
 ################################# SCRIPT FOR TESTING ###################################
 
-#params
-block_size = 40;
-search_r = 80;
-min_r = 0.50;
-mesh_length = 200;
-mesh_coeff = 0.5;
-match_coeff = 1.0;
-eta = 0.10
-n_steps = 120;
-n_grad = 80;
-show_plot = false;
-num_procs = 4;
-num_tiles = 16;
 
 Ms = MeshModule.makeNewMeshSet();
 sr = readdlm("section.txt");
 
-#meshArray = Array{Mesh, 1}(0);
-imageArray = Array{Array{Float64, 2}, 1}(0);
+#params
+block_size = 20;
+search_r = 80; #20;
+min_r = 0.65; #0.50;
+mesh_length = 200;
+mesh_coeff = 1;
+match_coeff = 10;
+eta = 0.01;
+n_steps = 2000;
+n_grad = 1950;
+show_plot = false;
+num_procs = 12;
+grad_threshold = 1/1000;
+n_newton = 50;
+max_tile_size = 10000;
+num_tiles = size(sr, 1);
 
-@time for i in 1:size(sr, 1)
+imageArray = SharedArray(Float64, 10000, 10000, num_tiles);
+
+@time for i in 1:num_tiles
 	path = string("./EM_images/", sr[i, 1]);
 	println(path);
 	di = sr[i, 2];
 	dj = sr[i, 3];
 	MeshModule.addMesh2MeshSet!(MeshModule.Tile2Mesh(path, (1, 20, i), (0, 0), di, dj, false, mesh_length, mesh_coeff), Ms);
 	meshImage = MeshModule.getMeshImage(Ms.meshes[i]);
-	push!(imageArray, meshImage);
+	imageArray[1:size(meshImage, 1), 1:size(meshImage, 2), i] = meshImage;
 end
-																																																																																																	
 
-
-#=
-PLoop = @sync @parallel for i=1:Ms.N, j in 1:Ms.N
-		if j == i continue; end
-	MeshModule.Meshes2Matches(imageArray[i], Ms.meshes[i], imageArray[j], Ms.meshes[j], block_size, search_r, min_r);
-end
-	for i in 1:size(PLoop)
-	MeshModule.addMatches2MeshSet!(fech(PLoop[i]), Ms);
+@time for k in 0:num_procs:Ms.N^2
+	toFetch = @sync @parallel for l in num_procs
+	(i, j) = (rem(k+l, Ms.N), cld(k+l, Ms.N));
+	if i == 0 i = Ms.N; end
+	if i == j || i > Ms.N || j > Ms.N continue end;
+		return MeshModule.Meshes2Matches(imageArray[:, :, i], Ms.meshes[i], imageArray[:, :, j], Ms.meshes[j], block_size, search_r, min_r);
 	end
-=#
+	for i = 1:length(toFetch)
+		MeshModule.addMatches2MeshSet!(fetch(toFetch[1]), Ms);
+	end
+end
+
 
 @time for i in 1:Ms.N
 	for j in 1:Ms.N
 		if j == i continue; end
-	M = MeshModule.Meshes2Matches(imageArray[i], Ms.meshes[i], imageArray[j], Ms.meshes[j], block_size, search_r, min_r);
+	@time M = MeshModule.Meshes2Matches(imageArray[i], Ms.meshes[i], imageArray[j], Ms.meshes[j], block_size, search_r, min_r);
 	if M.n == 0 continue; end
 	MeshModule.addMatches2MeshSet!(M, Ms);
 	end
@@ -75,5 +79,5 @@ Ms = makeNewMeshSet();
 @time MeshModule.addMatches2MeshSet!(Mab, Ms);
 @time MeshModule.addMatches2MeshSet!(Mba, Ms);
 =#
-@time MeshModule.solveMeshSet!(Ms, match_coeff, eta, n_steps, n_grad, show_plot);
+@time MeshModule.solveMeshSet!(Ms, match_coeff, eta, grad_threshold, n_newton);
 @time MeshModule.MeshSet2JLD("solvedMesh(1,20,0).jld", Ms);
