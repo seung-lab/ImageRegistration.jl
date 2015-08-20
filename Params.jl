b944220bf6625653ff231e4d47039a16ed285527
dodamih <dodam.a.ih@gmail.com>
2015-08-20 16:42:52 -0400

    committing change to the dataset path

--- Params.jl
+++ Params.jl
@@ -4,7 +4,7 @@ importall IO
 
 bucket_dir_path = "~/seungmount";
 affine_dir_path = "~";
-dataset_dir_path = "./piriform"
+dataset_dir_path = "./datasets/piriform"
 wafer_path_file = "wafer_paths.txt";
 info_path_file = "import_info.txt";
 
@@ -13,7 +13,7 @@ export BUCKET, DATASET_DIR, AFFINE_DIR, WAFER_DIR
 global BUCKET = bucket_dir_path;
 global AFFINE_DIR = affine_dir_path;
 global DATASET_DIR = dataset_dir_path;
-global WAFER_DIR = IO.waferpaths2dict(wafer_path_file);
+global WAFER_DIR = waferpaths2dict(wafer_path_file);
 
 export block_size, search_r, min_r, mesh_length, mesh_coeff, match_coeff, eta_grad, eta_newton, show_plot, num_procs, ftol_grad, ftol_newton, num_tiles, num_rows, num_cols;
 
