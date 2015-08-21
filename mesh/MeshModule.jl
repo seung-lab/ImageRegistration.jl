84772c7a5b3af2332fa55c6163fbb49458307b04
dodamih <dodam.ih@gmail.com>
2015-08-21 14:46:41 -0400

    added more IO functions

--- mesh/MeshModule.jl
+++ mesh/MeshModule.jl
@@ -2,6 +2,9 @@ module MeshModule
 
 importall Julimaps
 importall Params
+importall IO
+
+export montageSection, montageSections
 
 using Images
 using HDF5
@@ -11,6 +14,20 @@ include("Mesh.jl")
 include("Matches.jl")
 include("MeshSet.jl")
 include("MeshSolve.jl")
+
+function montageSection(n)
+	@time Ms, imageArray = MeshModule.loadSection(SESSION, n);
+	@time addAllMatches!(Ms, imageArray);
+	@time solveMeshSet!(Ms, match_coeff, eta_grad, eta_newton, ftol_grad, ftol_newton);
+	printResidualStats(Ms);
+	save(Ms);
+end
+
+function montageSections(k::UnitRange{Int64})
+	for i in k
+		montageSection(i);
+	end
+end
 
 
 
