31cc29ae84c31fe00e0676ee09fbc98ff967a3b6
dodamih <dodam.a.ih@gmail.com>
2015-08-21 11:47:28 -0400

    fixed mesh-related items

--- Params.jl
+++ Params.jl
@@ -43,15 +43,16 @@ global DATASET_DIR = dataset_dir_path;
 global WAFER_DIR_DICT = waferpaths2dict(joinpath(dataset_dir_path, wafer_filename));
 global SESSION = parseRoughAlign(joinpath(dataset_dir_path, rough_align_filename));
 
-export block_size, search_r, min_r, mesh_length, mesh_coeff, match_coeff, eta_grad, eta_newton, show_plot, num_procs, ftol_grad, ftol_newton, num_tiles, num_rows, num_cols;
+export tile_size, block_size, search_r, min_r, mesh_length, mesh_coeff, match_coeff, eta_grad, eta_newton, show_plot, num_procs, ftol_grad, ftol_newton, num_tiles, num_rows, num_cols;
 
-block_size = 50;
-search_r = 120;
+tile_size = 8000;
+block_size = 60;
+search_r = 80;
 min_r = 0.80;
-mesh_length = 100;
+mesh_length = 140;
 mesh_coeff = 1;
-match_coeff = 100;
-eta_grad = 0.001;
+match_coeff = 10;
+eta_grad = 0.01;
 eta_newton = .5; 
 show_plot = false;
 num_procs = length(procs());
