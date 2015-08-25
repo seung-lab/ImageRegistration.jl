282a31d5e47825793eb518e315dd0d62e2dd708b
Shang Mu <smu@princeton.edu>
2015-08-25 19:52:00 -0400

    Proof of good alignment

--- SectionAlignAffine.jl
+++ SectionAlignAffine.jl
@@ -4,6 +4,7 @@ include("convolve.jl")
 using Images
 include("imwarp.jl")
 include("visualize.jl")
+include("render.jl")
 
 
 function test()
@@ -13,37 +14,88 @@ function test()
 	sec2 = getimage("./sections/S2-W001_sec25_0.175.tif")
 	println(size(sec1))
 	println(size(sec2))
-	#trans, points1, points2 = AffineAlignSections(sec1, sec2, 0.175)
-	points, half_block_size, search_radius = GenerateMatchPoints(sec1, sec2)
-	points1, points2 = GetBlockMatches(sec1, sec2, points, half_block_size, search_radius, 0.3)
-	trans = FindAffine(points1, points2)
-	A = AdjustAffineForScaling(trans, 0.175)
+	trans, points1, points2, res1, res2 = AffineAlignSections(sec1, sec2, 0.175, return_points=true)
 
-	println(A)
+	println(trans)
 	#out_img, offset = imwarp(sec2, trans)
 	#imwrite(sec1, joinpath(".","test_outputs", string("sec1", ".tif")));
 	#imwrite(sec2, joinpath(".","test_outputs", string("sec2_", offset[1], "_", offset[2], ".tif")));
 	#imwrite(out_img, joinpath(".","test_outputs", string("warped_", offset[1], "_", offset[2], ".tif")));
+	p22 = points2 + res2;
+	p11 = points1 + res1;
 	points1 = [points1[2,:]; points1[1,:]]
 	points2 = [points2[2,:]; points2[1,:]]
+	draw_vectors(sec2, vcat(points2[1:2,:], p22[2:-1:1,:]))
+	#ccp.write_image_from_points(points1[1:2,:].', p11[2:-1:1,:].', "test_outputs/write_name.jpg")
 	draw_points(sec1, points1)
 	draw_points(sec2, points2)
 	#draw_points(out_img, points1)
 end
 
 
+function test2()
+	getimage(path) = convert(Array{Float64, 2}, convert(Array, imread(path)));
+	sec1 = getimage("./output_images/(1,1)_montage.tif")
+	sec2 = getimage("./output_images/(1,2)_montage.tif")
+	println(size(sec1))
+	println(size(sec2))
+	trans, points1, points2, res1, res2 = AffineAlignSections(sec1, sec2, 1, 0.3; return_points=true)
+
+	println(trans)
+	downsample = 4
+	sec1 = sec1[1:downsample:end, 1:downsample:end];
+	sec2 = sec2[1:downsample:end, 1:downsample:end];
+
+	p11 = points1 + res1;
+	p22 = points2 + res2;
+
+	p11 = p11[2:-1:1,:]
+	p22 = p22[2:-1:1,:]
+	points1 = points1[2:-1:1,:]
+	points2 = points2[2:-1:1,:]
+
+	p11 = p11/downsample;
+	p22 = p22/downsample;
+	points1 = points1/downsample;
+	points2 = points2/downsample;
+	
+	trans = AdjustAffineForScaling(trans.', downsample).'
+	out_img, offset = imwarp(sec2, inv(trans))
+	println(offset)
+	fused, fused_offset = imfuse(sec1, [0,0], out_img, offset)
+	println(fused_offset)
+	fused_offset = fused_offset[2:-1:1]
+
+	half_block_size = 150;
+	scalebar = [1; 1; 2*half_block_size/downsample; 2*half_block_size/downsample]
+	draw_vectors(make_isotropic(sec1), hcat(vcat(points1, p11), scalebar))
+	draw_vectors(make_isotropic(sec2), hcat(vcat(points2, p22), scalebar+100))
+	draw_vectors(make_isotropic(fused), hcat(vcat(points1.-fused_offset, p11.-fused_offset), scalebar)) # todo: check offset
+    #ccp.write_image_from_points(points1[1:2,:].', p11[2:-1:1,:].', "test_outputs/write_name.jpg")
+end
+
+
 """
 Returns the affine transformation A, such that p_in_B = p_in_A * A, where p is point coordinates in row vector.
 """
-function AffineAlignSections(img1::Array{}, img2::Array{}, downsample_ratio)
+function AffineAlignSections(img1::Array{}, img2::Array{}, downsample_ratio = 1, accept_xcorr = 0.3; return_points=false)
 	# points here are in column vector convention
 	points, half_block_size, search_radius = GenerateMatchPoints(img1, img2)
-	points1, points2 = GetBlockMatches(img1, img2, points, half_block_size, search_radius, 0.3)
+	points1, points2 = GetBlockMatches(img1, img2, points, half_block_size, search_radius, accept_xcorr)
 	A = FindAffine(points1, points2)
+	residualIn2 = A*points1 - points2;
+	points2in1 = inv(A)*points2;
+	residualIn1 = points2in1 - points1;
 	A = AdjustAffineForScaling(A, downsample_ratio)
 
 	# convert column vector convention to row vector convention
-	return A.'
+	A = A.'
+
+	if return_points
+		return A, points1, points2, residualIn1, residualIn2
+	else
+		return A
+	end
 end
 
 
@@ -61,7 +113,7 @@ function GenerateMatchPoints(img1::Array{}, img2::Array{})
 	println("search_radius: ", search_radius)
 
 	border = border > search_radius + half_block_size ? border : search_radius + half_block_size;
-	println(border)
+	println("border excluded: ", border)
 
 	# four corners for now
 	#points = [[x; y] for x = (1+border,size(img1,1)-border), y = (1+border,size(img1,2)-border)]
@@ -81,7 +133,7 @@ function GetBlockMatches(img1::Array{}, img2::Array{}, points, half_block_size, 
 	for p = points
 		offset,r = BlockMatchAtPoint(img1, p, img2, p, half_block_size, search_radius)
 		println(p, offset, r)
-		if r > accept_xcorr
+		if r >= accept_xcorr
 			push!(points1, [p; 1])
 			push!(points2, [p + offset; 1])
 		end
