a6e6c491cf4001c11d640d5ab486bb17b0454263
dodamih <dodam.a.ih@gmail.com>
2015-09-02 14:24:59 -0400

    new set of parameters

--- src/MeshSession.jl
+++ src/MeshSession.jl
@@ -3,7 +3,7 @@ function montage_section(n)
 	@time Ms, images = load_section(PREMONTAGED_OFFSETS, n);
 	@time add_all_matches!(Ms, images);
 	@time solve_meshset!(Ms);
-	printResidualStats(Ms);
+	print_res_stats(Ms);
 	save(Ms);
 	imageArray = 0;
 	gc();
@@ -19,12 +19,12 @@ end
 
 function align_stack(wafer_num, k::UnitRange{Int64})
 	@time Ms = make_stack(PREALIGNED_OFFSETS, wafer_num, k);
-	for i in 1:Ms.N-1
-	  a = Ms.meshes[i].index[2];
-	  b = Ms.meshes[i+1].index[2];
-	 add_pair_matches!(Ms, a, b); 
+	@time for i in 1:Ms.N-1
+	  @time a = Ms.meshes[i].index[2];
+	  @time b = Ms.meshes[i+1].index[2];
+	 @time add_pair_matches!(Ms, a, b); 
 	end
 	solve_meshset!(Ms);
-#	printResidualStats(Ms);
+	print_res_stats(Ms);
 	save(Ms);
 end
