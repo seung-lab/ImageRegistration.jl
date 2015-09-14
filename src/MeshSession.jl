b74b18ec9de9d977f535ab4e695f9590240a15d3
dodamih <dodam.ih@gmail.com>
2015-09-14 16:56:52 -0400

    Added support for fixed sections at specified intervals

--- src/MeshSession.jl
+++ src/MeshSession.jl
@@ -15,6 +15,16 @@ function montage_sections(wafer_num, k::UnitRange{Int64})
 
 end
 
+function align_stack(wafer_num, k::UnitRange{Int64}, fixed_interval)
+  @time Ms = make_stack(PREALIGNED_OFFSETS, wafer_num, k, fixed_interval);
+  @time for i in 1:Ms.N-1
+    @time a = Ms.meshes[i].index[2];
+    @time b = Ms.meshes[i+1].index[2];
+    @time add_pair_matches!(Ms, a, b); 
+  end
+  solve_meshset!(Ms);
+  save(Ms);
+end
 function align_stack(wafer_num, k::UnitRange{Int64})
   @time Ms = make_stack(PREALIGNED_OFFSETS, wafer_num, k);
   @time for i in 1:Ms.N-1
