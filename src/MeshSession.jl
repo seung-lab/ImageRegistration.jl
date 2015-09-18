661abbc5166be2cead4b081436bdd4c9a7e979e6
Dodam Ih <dodam.a.ih@gmail.com>
2015-09-18 17:01:01 -0400

    Update MeshSession.jl

--- src/MeshSession.jl
+++ src/MeshSession.jl
@@ -56,6 +56,17 @@ function prealign(wafer_num, dst, src)# k::UnitRange{Int64})
   return Ms;
 end
 
+function prealign(wafer_num_a, sec_num_a, wafer_num_b, sec_num_b)# k::UnitRange{Int64})
+if wafer_num_a != wafer_num_b println("No support for different wafers yet.") return; end
+for src in sec_num_a:sec_num_b
+dst = src - 1;
+  @time Ms = affine_make_stack(MONTAGED_OFFSETS, wafer_num_a, dst, src);
+  @time affine_add_pair_matches!(Ms, src, dst);
+  @time affine_solve_meshset!(Ms);
+  save(Ms);
+  end
+end
+
 function align_to_fixed(wafer_num, aligned, prealigned)
   @time Ms = make_stack(PREALIGNED_OFFSETS, wafer_num, aligned, prealigned);
   @time for i in 1:Ms.N-1
