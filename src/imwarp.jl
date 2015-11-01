8af9f2f3788ba60355cc9ad39f4cdb5e7b15c913
Thomas Macrina <thomas.macrina@gmail.com>
2015-10-31 20:14:02 -0400

    Update imwarp.jl
    Remove specific UInt8 handling in imwarp - covered by Integer.

--- src/imwarp.jl
+++ src/imwarp.jl
@@ -137,20 +137,3 @@ end
 function writepixel{T<:Ufixed8}(img::Array{T},i,j,pixelvalue)
     img[i,j]=pixelvalue
 end
-
-function writepixel{T<:UInt8}(img::Array{T},i,j,pixelvalue)
-    img[i,j]=pixelvalue
-end
-
-# Not used - slow.
-function bilinear(img, x, y)
-    fx, fy = floor(Int64, x), floor(Int64, y)
-    wx, wy = x-fx, y-fy
-    if 1 <= fx && fx+1 <= size(img, 1) && 1 <= fy && fy+1 <= size(img, 2)
-      # Expansion of p = [1-wx wx] * img[fx:fx+1, fy:fy+1] * [1-wy; wy]
-      p = ((1-wx)*img[fx,fy] + wx*img[fx+1,fy]) * (1-wy) + ((1-wx)*img[fx,fy+1] + wx*img[fx+1,fy+1]) * wy
-      return p
-    else
-      0
-    end
-end
