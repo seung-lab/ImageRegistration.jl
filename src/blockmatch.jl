0cba23f8533fc00968a91619a1a9bc9c2e242497
vsaase <vsaase@gmail.com>
2019-09-03 16:01:44 +0200

    registration fixes

--- src/blockmatch.jl
+++ src/blockmatch.jl
@@ -2,9 +2,9 @@ const NO_MATCH = ([0 0], -1)
 const NO_RANGE = (0:0, 0:0)
 
 mutable struct Matches
-  mesh_indices::Array{Int64, 1}
-  src_points::Array
-  dst_points::Array
+    mesh_indices::Array{Int64,1}
+    src_points::Array
+    dst_points::Array
 end
 
 """
@@ -20,32 +20,32 @@ end
 * correlogram: 2D array representing the cross corelation between A & B
 """
 function get_max_xc_vector(A, B)
-  if std(A) == 0 || std(B) == 0
-    return NO_MATCH, 0
-  end
-  xc = normxcorr2(A, B)
-  r_max = maximum(xc)
-  if isnan(r_max)
-    return NO_MATCH, 0
-  end
-  rad = round(Int64, (size(xc, 1) - 1)/ 2)
-  ind = findfirst(r_max .== xc)
-  x1 = size(xc, 1)
-  x2 = size(xc, 2)
-  if ind == 0
-    return NO_MATCH, 0
-  end
-  (i_max, j_max) = (rem(ind, size(xc, 1)), cld(ind, size(xc, 1)))
-  if i_max == 0
-    i_max = size(xc, 1)
-  end
-  return [i_max-1-rad j_max-1-rad], r_max, xc
+    if std(A) == 0 || std(B) == 0
+        return NO_MATCH, 0
+    end
+    xc = normxcorr2(A, B)
+    r_max = maximum(xc)
+    if isnan(r_max)
+        return NO_MATCH, 0
+    end
+    rad = round(Int64, (size(xc, 1) - 1) / 2)
+    ind = findfirst(r_max .== xc)
+    x1 = size(xc, 1)
+    x2 = size(xc, 2)
+    if ind == 0
+        return NO_MATCH, 0
+    end
+    (i_max, j_max) = (rem(ind, size(xc, 1)), cld(ind, size(xc, 1)))
+    if i_max == 0
+        i_max = size(xc, 1)
+    end
+    return [i_max - 1 - rad j_max - 1 - rad], r_max, xc
 end
 
 """
 `BLOCKMATCH` - Find block matches between two images at node locations.
 
-`matches = get_blockmatches(nodes, src_img, dst_img, src_offset, dst_offset, params)`
+`matches = blockmatch(nodes, src_img, dst_img, src_offset, dst_offset, params)`
 
 * nodes: Nx2 array of nodes for locations in an image where blockmatches will be
     conducted. Nodes are expected to be in global coordinates (not local to the
@@ -71,96 +71,96 @@ processors:
 
 """
 function blockmatch(nodes, src_img, dst_img, src_offset, dst_offset, params)
-  n = size(nodes, 1)
-  displacements = Array{Tuple{Array{Float64, 2}, Float64}, 1}(n)
-  src_ranges = Array{Tuple{UnitRange{Int64}, UnitRange{Int64}}, 1}(n)
-  dst_ranges = Array{Tuple{UnitRange{Int64}, UnitRange{Int64}}, 1}(n)
+    n = size(nodes, 1)
+    displacements = Array{Tuple{Array{Float64,2},Float64},1}(undef, n)
+    src_ranges = Array{Tuple{UnitRange{Int64},UnitRange{Int64}},1}(undef, n)
+    dst_ranges = Array{Tuple{UnitRange{Int64},UnitRange{Int64}},1}(undef, n)
 
-  block_size = params["block_size"]
-  search_r = params["search_r"]
-  min_r = params["min_r"]
-  b_rad = block_size + search_r
+    block_size = params["block_size"]
+    search_r = params["search_r"]
+    min_r = params["min_r"]
+    b_rad = block_size + search_r
 
-  src_sz = size(src_img)
-  dst_sz = size(dst_img)
+    src_sz = size(src_img)
+    dst_sz = size(dst_img)
 
-  for i in 1:n
-    pt = nodes[i]
-    src_ranges[i] = get_range(src_sz, pt, src_offset, block_size)
-    dst_ranges[i] = get_range(dst_sz, pt, dst_offset, b_rad)
-  end
+    for i = 1:n
+        pt = nodes[i]
+        src_ranges[i] = get_range(src_sz, pt, src_offset, block_size)
+        dst_ranges[i] = get_range(dst_sz, pt, dst_offset, b_rad)
+    end
 
-  num_procs = nprocs()
-  k = 1
-  nexti() = (i=k; k+=1; i)
-  @sync begin
-  for p in 1:num_procs
-    if p != myid() || num_procs == 1
-      @async begin
-      while true
-        i = nexti()
-        if i > n
-          break
+    num_procs = nprocs()
+    k = 1
+    nexti() = (i = k; k += 1; i)
+    @sync begin
+        for p = 1:num_procs
+            if p != myid() || num_procs == 1
+                @async begin
+                    while true
+                        i = nexti()
+                        if i > n
+                            break
+                        end
+                        if src_ranges[i] == NO_RANGE || dst_ranges[i] == NO_RANGE
+                            displacements[i] = NO_MATCH
+                            continue
+                        end
+
+                        src = src_img[src_ranges[i]...]
+                        dst = dst_img[dst_ranges[i]...]
+
+                        max_vect_xc = remotecall_fetch(get_max_xc_vector, p, src, dst)
+                        displacements[i] = max_vect_xc[1:2]
+                    end
+                end
+            end
         end
-        if src_ranges[i] == NO_RANGE || dst_ranges[i] == NO_RANGE
-          displacements[i] = NO_MATCH
-          continue
+    end
+
+    mesh_indices = []
+    src_points = []
+    dst_points = []
+    for i = 1:n
+        v = displacements[i]
+        if v == NO_MATCH
+            continue
         end
-
-        src = src_img[src_ranges[i]...]
-        dst = dst_img[dst_ranges[i]...]
-
-        max_vect_xc = remotecall_fetch(get_max_xc_vector, p, src,  dst)
-        displacements[i] = max_vect_xc[1:2]
-      end
-      end
+        if v[2] < min_r
+            continue
+        end
+        push!(mesh_indices, i)
+        push!(src_points, nodes[i, :])
+        push!(dst_points, nodes[i, :] + v[1])
     end
-  end
-  end
-
-  mesh_indices = []
-  src_points = []
-  dst_points = []
-  for i in 1:n
-    v = displacements[i]
-    if v == NO_MATCH
-      continue
+    if length(dst_points) == 0
+        return Nothing
     end
-    if v[2] < min_r
-      continue
-    end
-    push!(mesh_indices, i)
-    push!(src_points, nodes[i, :])
-    push!(dst_points, nodes[i, :] + v[1])
-  end
-  if length(dst_points) == 0
-    return Void
-  end
-  return Matches(mesh_indices, vcat(src_points...), vcat(dst_points...))
+    return Matches(mesh_indices, vcat(src_points...), vcat(dst_points...))
 end
 
 """
 Is a point contained within an image perimeter, less a certain radius?
 """
 function is_internal(sz, pt, radius)
-  above_min_i = pt[1] > radius
-  above_min_j = pt[2] > radius
-  below_max_i = pt[1] <= (sz[1]-radius)
-  below_max_j = pt[2] <= (sz[2]-radius)
-  return above_min_i && above_min_j && below_max_i && below_max_j
+    above_min_i = pt[1] > radius
+    above_min_j = pt[2] > radius
+    below_max_i = pt[1] <= (sz[1] - radius)
+    below_max_j = pt[2] <= (sz[2] - radius)
+    return above_min_i && above_min_j && below_max_i && below_max_j
 end
 
 """
 From global point and offset, calculate range of pixels within radius to slice
 """
 function get_range(sz, pt, offset, radius)
-  if !is_internal(sz, pt-offset, radius)
-    return NO_RANGE
-  end
-  pt_local = pt-offset
-  i = round(Int64, ceil(pt_local[1]))
-  j = round(Int64, ceil(pt_local[2]))
-  i_range = i-radius:i+radius
-  j_range = j-radius:j+radius
-  return i_range, j_range
+    if !is_internal(sz, pt .- offset, radius)
+        return NO_RANGE
+    end
+    pt_local = pt .- offset
+    i = round(Int64, ceil(pt_local[1]))
+    j = round(Int64, ceil(pt_local[2]))
+    i_range = i-radius:i+radius
+    j_range = j-radius:j+radius
+    return i_range, j_range
 end
--- src/boundingbox.jl
+++ src/boundingbox.jl
@@ -1,75 +1,71 @@
 import Base: +, -, ==
 
 struct BoundingBox{T}
-  i::T
-  j::T
-  h::T    # height
-  w::T    # width
+    i::T
+    j::T
+    h::T    # height
+    w::T    # width
 end
 
 eps = 1e-6
 
-BoundingBox() = BoundingBox(0,0,0,0)
-BoundingBox(a, b, c, d) = BoundingBox(promote(a,b,c,d)...)
+BoundingBox() = BoundingBox(0, 0, 0, 0)
+BoundingBox(a, b, c, d) = BoundingBox(promote(a, b, c, d)...)
 
 """
 Add bounding boxes to find BB of their union
 """
 function +(bbA::BoundingBox, bbB::BoundingBox)
-  i = min(bbA.i, bbB.i)
-  j = min(bbA.j, bbB.j)
-  h = max(bbA.h+bbA.i, bbB.h+bbB.i) - i
-  w = max(bbA.w+bbA.j, bbB.w+bbB.j) - j
-  return BoundingBox(i,j,h,w)
+    i = min(bbA.i, bbB.i)
+    j = min(bbA.j, bbB.j)
+    h = max(bbA.h + bbA.i, bbB.h + bbB.i) - i
+    w = max(bbA.w + bbA.j, bbB.w + bbB.j) - j
+    return BoundingBox(i, j, h, w)
 end
 
 """
 Subtract bounding boxes to find BB of their intersection
 """
 function -(bbA::BoundingBox, bbB::BoundingBox)
-  i = max(bbA.i, bbB.i)
-  j = max(bbA.j, bbB.j)
-  h = min(bbA.h+bbA.i, bbB.h+bbB.i)-i
-  w = min(bbA.w+bbA.j, bbB.w+bbB.j)-j
-  if h <= 0 || w <= 0
-    bb = BoundingBox(NaN, NaN, NaN, NaN)
-  else
-    bb = BoundingBox(i,j,h,w)
-  end
-  return bb
+    i = max(bbA.i, bbB.i)
+    j = max(bbA.j, bbB.j)
+    h = min(bbA.h + bbA.i, bbB.h + bbB.i) - i
+    w = min(bbA.w + bbA.j, bbB.w + bbB.j) - j
+    if h <= 0 || w <= 0
+        bb = BoundingBox(NaN, NaN, NaN, NaN)
+    else
+        bb = BoundingBox(i, j, h, w)
+    end
+    return bb
 end
 
 """
 Boolean if bounding boxes intersect
 """
 function intersects(bbA::BoundingBox, bbB::BoundingBox)
-  bb = bbA - bbB
-  return !isnan(bb.i)
+    bb = bbA - bbB
+    return !isnan(bb.i)
 end
 
 """
 Test if BoundingBoxes have the same origin and dimensions
 """
 function ==(bbA::BoundingBox, bbB::BoundingBox)
-  return bbA.i == bbB.i && bbA.j == bbB.j && bbA.w == bbB.w && bbA.h == bbB.h
+    return bbA.i == bbB.i && bbA.j == bbB.j && bbA.w == bbB.w && bbA.h == bbB.h
 end
 
 """
 Convert bounding box object to a polygon point list (counter-clockwise)
 """
 function bb_to_pts(r)
-  return [r.i r.j;
-          r.i+r.h r.j;
-          r.i+r.h r.j+r.w;
-          r.i r.j+r.w;
-          r.i r.j];
+    return [r.i r.j; r.i + r.h r.j; r.i + r.h r.j + r.w; r.i r.j + r.w; r.i r.j]
 end
 
 """
 Return xmin, ymin, xmax, ymax of a bounding box (opposing corner definition)
 """
 function minsandmax(bb)
-  return bb.i, bb.j, bb.i+bb.h, bb.j+bb.w
+    return bb.i, bb.j, bb.i + bb.h, bb.j + bb.w
 end
 
 """
@@ -82,11 +78,11 @@ end
 
 """
 function find_mesh_bb(nodes)
-    ilow = floor(Int64,minimum(nodes[1,:]))
-    jlow = floor(Int64,minimum(nodes[2,:]))
-    ihigh = ceil(Int64,maximum(nodes[1,:]))
-    jhigh = ceil(Int64,maximum(nodes[2,:]))
-    return BoundingBox(ilow, jlow, ihigh-ilow, jhigh-jlow)
+    ilow = floor(Int64, minimum(nodes[1, :]))
+    jlow = floor(Int64, minimum(nodes[2, :]))
+    ihigh = ceil(Int64, maximum(nodes[1, :]))
+    jhigh = ceil(Int64, maximum(nodes[2, :]))
+    return BoundingBox(ilow, jlow, ihigh - ilow, jhigh - jlow)
 end
 
 """
@@ -94,60 +90,60 @@ Snap bounding box to integer values (smallest rectangle containing original)
 Returns BoundingBox of integers
 """
 function snap_bb(bb)
-  r = bb_to_pts(bb)
-  i = floor(Int,bb.i)
-  j = floor(Int,bb.j)
-  h = ceil(Int,r[2,1]) - i
-  w = ceil(Int,r[3,2]) - j
-  return BoundingBox{Int}(i, j, h, w)
+    r = bb_to_pts(bb)
+    i = floor(Int, bb.i)
+    j = floor(Int, bb.j)
+    h = ceil(Int, r[2, 1]) - i
+    w = ceil(Int, r[3, 2]) - j
+    return BoundingBox{Int}(i, j, h, w)
 end
 
 """
 Apply affine transform to points in a bounding box & find new bounding box
 """
 function tform_bb(bb, tform)
-  tform_pts = [bb_to_pts(bb) ones(size(bb_to_pts(bb),1),1)] * tform
-  i = minimum(tform_pts[:,1])
-  j = minimum(tform_pts[:,2])
-  h = maximum(tform_pts[:,1])-i
-  w = maximum(tform_pts[:,2])-j
-  return BoundingBox(i, j, h, w)
+    tform_pts = [bb_to_pts(bb) ones(size(bb_to_pts(bb), 1), 1)] * tform
+    i = minimum(tform_pts[:, 1])
+    j = minimum(tform_pts[:, 2])
+    h = maximum(tform_pts[:, 1]) - i
+    w = maximum(tform_pts[:, 2]) - j
+    return BoundingBox(i, j, h, w)
 end
 
 """
 Convert Tuple for image size into a BoundingBox at (1,1)
 """
 function sz_to_bb(sz)
-  return BoundingBox(0, 0, sz[1], sz[2])
+    return BoundingBox(0, 0, sz[1], sz[2])
 end
 
 """
 Convert a BoundingBox to its snapped sizes
 """
 function bb_to_sz(bb)
-  bb = snap_bb(bb);
-  return bb.h, bb.w
+    bb = snap_bb(bb)
+    return bb.h, bb.w
 end
 
 """
 Get bounding box offset
 """
 function get_offset(bb::BoundingBox)
-  return [bb.i, bb.j]
+    return [bb.i, bb.j]
 end
 
 """
 Get height & width of bounding box
 """
 function get_size(bb::BoundingBox)
-  return [bb.h, bb.w]
+    return [bb.h, bb.w]
 end
 
 """
 Get four-tuple of bounding box upper-left & lower-right in i,j coordinates
 """
 function get_bounds(bb::BoundingBox)
-  return (bb.i, bb.j, bb.i+bb.h, bb.j+bb.w)
+    return (bb.i, bb.j, bb.i + bb.h, bb.j + bb.w)
 end
 
 """
@@ -156,56 +152,60 @@ Get four-tuple of bounding box upper-left & dimensions in x,y coordinates
 Convention used by Cairo package
 """
 function get_rect(bb::BoundingBox)
-  return (bb.j, bb.i, bb.w, bb.h)
+    return (bb.j, bb.i, bb.w, bb.h)
 end
 
 """
 Return the product of the width & height of bounding box
 """
 function get_area(bb::BoundingBox)
-  return bb.w*bb.h
+    return bb.w * bb.h
 end
 
 """
 Convert bounding box to tuple of ranges for easy array slicing
 """
 function bb_to_slice(bb::BoundingBox{Int64})
-  return (bb.i):(bb.i+bb.h-1), (bb.j):(bb.j+bb.w-1)
+    return (bb.i):(bb.i+bb.h-1), (bb.j):(bb.j+bb.w-1)
 end
 
 function bb_to_slice(bb::BoundingBox{Float64})
-  return round(Int64, bb.i):round(Int64, bb.i+bb.h-1),
-              round(Int64, bb.j):round(Int64, bb.j+bb.w-1)
+    return round(Int64, bb.i):round(Int64, bb.i + bb.h - 1),
+        round(Int64, bb.j):round(Int64, bb.j + bb.w - 1)
 end
 
 """
 Convert tuple of ranges to bounding box
 """
 function slice_to_bb(slice)
-  return BoundingBox(slice[1][1], slice[2][1],
-                      slice[1][end]-slice[1][1]+1, slice[2][end]-slice[2][1]+1)
+    return BoundingBox(
+        slice[1][1],
+        slice[2][1],
+        slice[1][end] - slice[1][1] + 1,
+        slice[2][end] - slice[2][1] + 1
+    )
 end
 
 """
 Shift bounding box by 2-element array
 """
 function translate_bb(bb::BoundingBox, offset)
-  return BoundingBox(bb.i + offset[1], bb.j + offset[2], bb.h, bb.w)
+    return BoundingBox(bb.i + offset[1], bb.j + offset[2], bb.h, bb.w)
 end
 
 """
 Transform bounding box by scaling matrix and snap for nearest integer
 """
 function scale_bb(bb::BoundingBox{Int64}, scale)
-  tform = make_scale_matrix(scale)
-  tbb = tform_bb(bb, tform)
-  return snap_bb(tbb)
+    tform = make_scale_matrix(scale)
+    tbb = tform_bb(bb, tform)
+    return snap_bb(tbb)
 end
 
 """
 Transform bounding box by scaling matrix
 """
 function scale_bb(bb::BoundingBox{Float64}, scale)
-  tform = make_scale_matrix(scale)
-  return tform_bb(bb, tform)
+    tform = make_scale_matrix(scale)
+    return tform_bb(bb, tform)
 end
